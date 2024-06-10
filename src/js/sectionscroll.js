650cd26ca8de67eacdfc5b466cc0430cc3397593
the-muckies <muckymahem@gmail.com>
2024-06-10 09:17:08 +0200

    Refactoring Single Page Layout

---
+++ src/js/sectionscroll.js
@@ -0,0 +1,18 @@
+// const navLinkEls = document.querySelectorAll( '.nav-link' );
+const sectionEls = document.querySelectorAll( '.section' );
+
+let currentSection = 'home';
+window.addEventListener( 'scroll', () => {
+    sectionEls.forEach( sectionEl => {
+        if ( window.scrollY >= ( sectionEl.offsetTop - sectionEl.clientHeight / 3 )) {
+            currentSection = sectionEl.id;
+        }
+    });
+console.log(`Currently in Section: ${currentSection}`);
+    navLinkEls.forEach( navLinkEl => {
+        if ( navLinkEl.href.includes( currentSection )) {
+            document.querySelector( 'active' ).classList.remove( 'active' );
+            navLinkEl.classList.add( 'active' );
+        }
+    });
+});
