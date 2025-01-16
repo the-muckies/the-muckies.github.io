68dc877b53ac0d29eeb043dfab451eabfe6a8a1e
the-muckies <muckymahem@gmail.com>
2025-01-16 09:15:53 +0100

    v1.3.3 Published
    Where To Next Updated
    Ride Diary Updated

--- src/js/utils.js
+++ src/js/utils.js
@@ -4,7 +4,7 @@ let minItem = document.querySelector("#min");
 let secItem = document.querySelector("#sec");
 
 let countDown = () => {
-    let futureDate = new Date("2024-10-26T09:30:00");
+    let futureDate = new Date("2025-02-22T09:30:00");
     let currentDate = new Date();
     let myDate = futureDate - currentDate;
     console.log(myDate);
